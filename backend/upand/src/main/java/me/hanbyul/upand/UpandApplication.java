e3f051ebe56dce90959d3b360debdba7b93dcc6a
HANBYUL <kyonggi@kakao.com>
2023-04-29 21:07:21 +0900

    add: install spring boot 3.0.6

---
+++ backend/upand/src/main/java/me/hanbyul/upand/UpandApplication.java
@@ -0,0 +1,13 @@
+package me.hanbyul.upand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpandApplication.class, args);
+	}
+
+}
