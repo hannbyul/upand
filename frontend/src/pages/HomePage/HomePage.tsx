5f141d43c29da394402997b111d0a8af58e20228
HANBYUL <kyonggi@kakao.com>
2023-05-02 20:25:23 +0900

    add: simple home layout

---
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import NavigationHeader from "../../components/layout/NavigationHeader/NavigationHeader";
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavigationHeader />
+    </div>
+  );
+};
+
+export default HomePage;
